6d219f109d3ea7d6b4b6822dff2f4893d3027ba1
yohelwirth <yohelwirthcouto@gmail.com>
2023-11-27 21:03:49 -0300

    aplicacion de funciones para la segunda entrega

--- js/index.js
+++ js/index.js
@@ -1,22 +1,56 @@
-let moneda1 = prompt("Desea comprar dólares, euros o reales?").toLowerCase();
+function TipoMoneda() {
+    let moneda = prompt("Desea comprar dólares, euros o reales?").toLowerCase();
+    return moneda;
+}
 
-if (moneda1 === "dolar" || moneda1 === "dolares" || moneda1 === "euro" || moneda1 === "euros" || moneda1 === "real" || moneda1 === "reales") {
-    let cantidadVeces = parseInt(prompt("¿Cuántas veces desea cotizar " + moneda1 + "?"));
+function CantidadVeces() {
+    let cantidadVeces = parseInt(prompt("¿Cuántas veces desea cotizar " + moneda + "?"));
+    return cantidadVeces;
+}
+
+function CantidadMoneda(cotizacion) {
+    let cantidadMoneda = parseInt(prompt("Cuantos " + moneda + " desea comprar (cotización " + cotizacion + "):"));
+    return cantidadMoneda;
+}
+
+function convertirMoneda(cantidad, tipoMoneda) {
+    let conversion;
+    switch (tipoMoneda) {
+        case "dolar":
+        case "dolares":
+            conversion = cantidad * 41;
+            break;
+        case "euro":
+        case "euros":
+            conversion = cantidad * 47;
+            break;
+        case "real":
+        case "reales":
+            conversion = cantidad * 8;
+            break;
+        default:
+            conversion = 0;
+            break;
+    }
+    return conversion;
+}
+
+function mostrarResultado(cantidad, tipoMoneda, conversion) {
+    alert(cantidad + " " + tipoMoneda + " serían UYU$" + conversion);
+}
+
+let moneda = obtenerTipoMoneda();
+
+if (moneda === "dolar" || moneda === "dolares" || moneda === "euro" || moneda === "euros" || moneda === "real" || moneda === "reales") {
+    let cantidadVeces = obtenerCantidadVeces();
 
     if (!isNaN(cantidadVeces)) {
         for (let i = 1; i <= cantidadVeces; i++) {
-            let cantidadMoneda = parseInt(prompt("Cuantos " + moneda1 + " desea comprar (cotización " + i + "):"));
+            let cantidadMoneda = obtenerCantidadMoneda(i);
 
             if (!isNaN(cantidadMoneda)) {
-                let conversion;
-                if (moneda1 === "dolar" || moneda1 === "dolares") {
-                    conversion = cantidadMoneda * 41;
-                } else if (moneda1 === "euro" || moneda1 === "euros") {
-                    conversion = cantidadMoneda * 47;
-                } else if (moneda1 === "real" || moneda1 === "reales") {
-                    conversion = cantidadMoneda * 8;
-                }
-                alert(cantidadMoneda + " " + moneda1 + " serían UYU$" + conversion);
+                let conversion = convertirMoneda(cantidadMoneda, moneda);
+                mostrarResultado(cantidadMoneda, moneda, conversion);
             } else {
                 alert("Cantidad no válida en la cotización " + i);
             }
@@ -25,5 +59,5 @@ if (moneda1 === "dolar" || moneda1 === "dolares" || moneda1 === "euro" || moneda
         alert("Cantidad de cotizaciones no válida");
     }
 } else {
-    alert(moneda1 + " no es una moneda válida");
+    alert(moneda + " no es una moneda válida");
 }
